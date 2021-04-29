46e710c5cec61d7bdfb5695f156aa680cf9ac968
c.pantoja <c.pantoja@uniandes.edu.co>
2021-04-29 01:33:13 -0500

    file structure and bottom navigation widget

---
+++ noboto_app/lib/src/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:noboto_app/src/views/ui/home.dart';
+
+class NobotoApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Noboto',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
