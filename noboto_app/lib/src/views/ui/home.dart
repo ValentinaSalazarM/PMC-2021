64edda15bd404a013132998227fa30fe80f73a09
c.pantoja <c.pantoja@uniandes.edu.co>
2021-04-29 11:05:03 -0500

    Relocate bottom navigation widget

--- noboto_app/lib/src/views/ui/home.dart
+++ noboto_app/lib/src/views/ui/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:noboto_app/src/views/utils/app_icons.dart';
+import 'package:noboto_app/src/views/utils/navigation.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -34,65 +34,10 @@ class _HomeState extends State<Home> {
                           child: Icon(Icons.add),
                           elevation: 0.1,
                         )),
-                    Container(
-                      width: size.width,
-                      height: 80,
-                      child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            IconButton(
-                              icon: Icon(AppIcons.exchange_alt),
-                              onPressed: () {},
-                            ),
-                            IconButton(
-                              icon: Icon(AppIcons.gift),
-                              onPressed: () {},
-                            ),
-                            Container(
-                              width: size.width * 0.2,
-                            ),
-                            IconButton(
-                              icon: Icon(AppIcons.hand_holding),
-                              onPressed: () {},
-                            ),
-                            IconButton(
-                              icon: Icon(Icons.messenger_rounded),
-                              onPressed: () {},
-                            )
-                          ]),
-                    )
+                    BottomNavigation(size: size)
                   ]),
                 ))
           ],
         ));
-  }
-}
-
-class BNBCustomPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    Paint paint = Paint()
-      ..color = Colors.white
-      ..style = PaintingStyle.fill;
-    Path path = Path()..moveTo(0, 0);
-    // path.quadraticBezierTo(size.width * 0.20, 0,size.width * 0.35, 0);
-    path.lineTo(size.width * 0.35, 0);
-    path.quadraticBezierTo(size.width * 0.40, 0, size.width * 0.40, 20);
-    path.arcToPoint(Offset(size.width * 0.60, 20),
-        radius: Radius.circular(10.0), clockwise: false);
-    path.quadraticBezierTo(size.width * 0.60, 0, size.width * 0.65, 0);
-    // path.quadraticBezierTo(size.width * 0.80, 0, size.width, 20);
-    path.lineTo(size.width, 0);
-    path.lineTo(size.width, size.height);
-    path.lineTo(0, size.height);
-    path.close();
-    canvas.drawShadow(path, Colors.black, 5, true);
-
-    canvas.drawPath(path, paint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return false;
   }
 }
