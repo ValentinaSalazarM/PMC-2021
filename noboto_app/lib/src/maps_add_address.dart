c6b098de95284a15330839875fb63463ccecd758
camilodi1999 <60122434+camilodi1999@users.noreply.github.com>
2021-05-10 08:56:38 -0500

    pantalla add address

--- noboto_app/lib/src/maps_add_address.dart
+++ noboto_app/lib/src/maps_add_address.dart
@@ -28,40 +28,72 @@ class _GoogleMapScreenState extends State<GoogleMapScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(children: [
-      GoogleMap(
-        //onMapCreated: _onMapCreated,
-        markers: Set.from(_markers),
-        initialCameraPosition: CameraPosition(target: actual, zoom: 15),
-      ),
-      Positioned(
-        top: 30.0,
-        right: 15.0,
-        left: 15,
-        child: Container(
-          height: 50.0,
-          width: double.infinity,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10), color: Colors.white),
-          child: TextField(
-            decoration: InputDecoration(
-                hintText: 'Ingrese su dirección',
-                border: InputBorder.none,
-                contentPadding: EdgeInsets.only(left: 15, top: 15),
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.search),
-                  onPressed: searchandNavigate,
-                  iconSize: 30,
-                )),
-            onChanged: (val) {
-              setState(() {
-                searchAddr = val;
-              });
-            },
+        appBar: AppBar(
+          title: Text("Cambiar dirección"),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back_ios_rounded),
+            tooltip: 'Atrás',
+            onPressed: () {},
           ),
         ),
-      )
-    ]));
+        body: Stack(children: [
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            markers: Set.from(_markers),
+            initialCameraPosition: CameraPosition(target: actual, zoom: 15),
+          ),
+          Positioned(
+            top: 30.0,
+            right: 15.0,
+            left: 15,
+            child: Container(
+              height: 50.0,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10), color: Colors.white),
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: 'Ingrese su dirección',
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.only(left: 15, top: 15),
+                    suffixIcon: IconButton(
+                      icon: Icon(Icons.search),
+                      onPressed: searchandNavigate,
+                      iconSize: 30,
+                    )),
+                onChanged: (val) {
+                  setState(() {
+                    searchAddr = val;
+                  });
+                },
+              ),
+            ),
+          )
+        ]),
+        bottomNavigationBar: Stack(
+          children: [
+            Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: TextButton(
+                      style: TextButton.styleFrom(
+                        primary: Colors.white,
+                        backgroundColor: Color.fromRGBO(31, 58, 131, 1),
+                        textStyle: TextStyle(
+                          fontSize: 18,
+                        ),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50.0)),
+                      ),
+                      onPressed: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("Guardar"),
+                      )),
+                )),
+          ],
+        ));
   }
 
   searchandNavigate() {
